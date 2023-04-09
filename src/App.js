49301ae6a166f5f685fbb16ff3c759e0a35ec746
Shariful4447 <shariful.islam4447@gmail.com>
2023-04-10 00:08:22 +0200

    Form validation Data from input field

--- src/App.js
+++ src/App.js
@@ -57,6 +57,7 @@ const handleSignout = () => {
     const signedOutuser = {isSignedIn:false,
       name: "",
       photo:"",
+      password:"",
       email:""
     }
     setUser(signedOutuser);
@@ -69,17 +70,29 @@ const handleSignout = () => {
 }
 
 const handleBlur = (e) => {
-  console.log(e.target.name, e.target.value);
-  if (e.target.name === "email") {
-    const isEmailValid = /\S+@\S+\.\S+/.test(e.target.value);
-    console.log(isEmailValid);
-  }
-  if (e.target.name === "password") {
+    let isFormValid = true;
+    if (e.target.name === "email") {
+      isFormValid = /\S+@\S+\.\S+/.test(e.target.value);
+      
+    }
+    if (e.target.name === "password") {
+      const isPasswordValid=e.target.value.length >= 6;
+      const passwordHasNumber = /\d{1}/.test(e.target.value);
 
-    const isPasswordValid = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(e.target.value);
-    console.log(isPasswordValid);
+      //const isFormValid = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(e.target.value);
+      isFormValid = isPasswordValid && passwordHasNumber;
     
-  }
+    
+    }
+    if (isFormValid) {
+      
+      const newUserInfo={...user};
+      newUserInfo[e.target.name] = e.target.value;
+      setUser(newUserInfo);
+
+
+      
+    }
 }
 
 const handleSubmit = () => {
@@ -103,12 +116,16 @@ const handleSubmit = () => {
       }
     
     <h1>New Authentication : </h1>
+    <p>Name : {user.name}</p>
+    <p>Email : {user.email}</p>
+    <p>Password : {user.password}</p>
     
     <form onSubmit={handleSubmit}>
+      <input name="name" type="text" onBlur={handleBlur} placeholder='Your Name'/>
     <br />
     <input type="text" onBlur={handleBlur} name="email" placeholder='Write Your Email Address' required />
     <br />
-    <input type="password" onChange={handleBlur} name="password" id="" placeholder='Your Password' required/>
+    <input type="password" onBlur={handleBlur} name="password" id="" placeholder='Your Password' required/>
     <br />
     
     <input type="submit" value="Submit" />
